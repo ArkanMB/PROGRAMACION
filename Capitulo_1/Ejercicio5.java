511bb6c380b46bac99a8bc59decf4bdd6c96bfd1
ArkanMB <arkangel@Arkangel.local>
2022-11-13 21:36:57 +0100

    Tareas de Clase

---
+++ Capitulo_1/Ejercicio5.java
@@ -0,0 +1,23 @@
+/* 
+*  José Ángel Moya Baena
+*/
+public class Ejercicio5 {
+    public static void main(String[] args) {
+    System.out.println("┌─────────────────┬───────────┬────────────┬─────────────┬───────────┬───────────┐");
+    System.out.println("│      Horas      │   Lunes   │   Martes   │  Miercoles  │  Jueves   │  Viernes  │");
+    System.out.println("├─────────────────┼───────────┼────────────┼─────────────┼───────────┼───────────┤");
+    System.out.print("│   15.15/16.15   │\033[31m    FOL    \033[39;49m│\033[39;35m");
+    System.out.println("  BASE DT.  \033[39;49m│\033[35m  BASE DT.   \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;36m LEN.MARC. \033[39;49m│\033[39;49m");
+    System.out.print("│   16.15/17.15   \033[39;49m│\033[31m    FOL    \033[39;49m│\033[39;35m");
+    System.out.println("  BASE DT.  \033[39;49m│\033[35m  BASE DT.   \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;36m LEN.MARC. \033[39;49m│\033[39;49m");
+    System.out.print("│   17.15/18.15   \033[39;49m│\033[31m    FOL    \033[39;49m│\033[39;32m");
+    System.out.println("  PROGRAM.  \033[39;49m│\033[35m  BASE DT.   \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[39;49m");
+    System.out.print("│   18.30/19.30   \033[39;49m│\033[33m ENT.DES.  \033[39;49m│\033[39;32m");
+    System.out.println("  PROGRAM.  \033[39;49m│\033[35m  BASE DT.   \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[39;49m");
+    System.out.print("│   19.30/20.30   \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;36m");
+    System.out.println("  LEN.MARC. \033[39;49m│\033[33m  ET.DES.    \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;49m");
+    System.out.print("│   20.30/21.30   \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;36m");
+    System.out.println("  LEN.MARC. \033[39;49m│\033[33m  ET.DES.    \033[39;49m│\033[39;32m  PROGRAM. \033[39;49m│\033[34m SIST.INF. \033[39;49m│\033[39;49m");
+    System.out.println("└─────────────────┴───────────┴────────────┴─────────────┴───────────┴───────────┘");
+    }
+}
