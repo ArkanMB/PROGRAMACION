b8cab998f7130a8ff74a535913b6cc4c4062e4cc
ArkanMB <arkangel@Arkangel.local>
2022-11-15 17:32:59 +0100

    Tareas Java

---
+++ Capitulo3/Ej1.java
@@ -0,0 +1,16 @@
+/*
+ * Ej1.java
+ * 
+ * Ejercicio 1: Multiplica dos números 
+ * 
+ * @Ángel
+ */
+
+ public class Ej1{
+    public static void main(String[] args) {
+        
+        
+   
+    }
+
+ }
