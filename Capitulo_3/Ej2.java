550763c33eca82ccc9a89c4d2f5f92b1b23e16d1
ArkanMB <arkangel@Arkangel.local>
2022-11-17 21:07:31 +0100

    Tareas clase

---
+++ Capitulo_3/Ej2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+ * Ej2.java
+ * 
+ * El ejercicio recoje los euros que quieras convertir a pesetas
+ * 
+ * @Ángel
+ */
+
+public class Ej2{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Escriba los euros que quieras convertir a pesetas => ");
+        int euros = s.nextInt();
+
+        int pesetas = (int) euros * 166;
+
+        System.out.println(euros + " son: " + pesetas + " pesetas.");
+
+    }
+
+
+
+}
