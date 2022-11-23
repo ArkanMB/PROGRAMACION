321b0324fcc9df3d074917a9821111f9634ebcfb
ArkanMB <arkangel@Arkangel.local>
2022-11-23 21:14:50 +0100

    Actualizaciones de las tareas .java

--- Capitulo_3/Ej8.java
+++ Capitulo_3/Ej8.java
@@ -9,16 +9,16 @@ import java.util.Scanner;
  */
 
  public class Ej8 {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
 
-        System.out.print("Introduzca el número de horas trabajadas durante la semana => ");
+    System.out.print("Introduzca el número de horas trabajadas durante la semana => ");
 
-        int horas = s.nextInt(); 
+    int horas = s.nextInt(); 
 
-        int salario = horas * 12; 
+    int salario = horas * 12; 
 
-        System.out.println("El salario semanal sería: " + salario + " € ");
+    System.out.println("El salario semanal sería: " + salario + " € ");
 
-    }
+  }
  }
