018d52b4cb447ee8c4332e7f6e852e9535c20d97
ArkanMB <arkangel@Arkangel.local>
2023-04-17 19:21:10 +0200

    Tareas java

--- Capitulo_6/C6.1/C6Ejemplos.java
+++ Capitulo_6/C6.1/C6Ejemplos.java
@@ -7,54 +7,53 @@
  * @ÁngelMB
 */
 
-
 public class C6Ejemplos {
   public static void main(String[] args) {
-		System.out.println(" ");
-		System.out.println("Esto son números aleatorios del 0 al 9 => ");
-		System.out.println(" ");
-		
-		for (int veces = 1; veces <= 15; veces = veces + 1){
-			System.out.print( (int)(Math.random()*10) + " ");
-		}
-		System.out.println(" ");
     System.out.println(" ");
-		System.out.println(" ");
-		System.out.println("Esto son números aleatorios del 1 al 10 => ");
-		System.out.println(" ");
-		
-		for (int veces = 1; veces <= 15; veces = veces + 1){
-			System.out.print( (int)(Math.random()*10+1) + " ");
-		}
-		System.out.println(" ");
+    System.out.println("Esto son números aleatorios del 0 al 9 => ");
     System.out.println(" ");
-		System.out.println(" ");
-		System.out.println("Esto son números aleatorios del 0 al 10 => ");
-		System.out.println(" ");
-		
-		for (int veces = 1; veces <= 15; veces = veces + 1){
-			System.out.print( (int)(Math.random()*11) + " ");
-		}
-		System.out.println(" ");
-    System.out.println(" ");
-		System.out.println(" ");
-		System.out.println("Esto son números aleatorios del 0 al 6 => ");
-		System.out.println(" ");
-		
-		for (int veces = 1; veces <=15; veces = veces + 1){
-			System.out.print( (int)(Math.random()*7) + " ");
-		}
-		System.out.println(" ");
-		System.out.println(" ");
-    System.out.println(" ");
-		System.out.println("Esto son números aleatorios del 50 al 60 => ");
-		System.out.println(" ");
-		
-		for (int veces = 1; veces <=15; veces = veces + 1){
-			System.out.print( ((int)(Math.random()*11)+50) + " ");
-      
-		}
+    
+    for (int veces = 1; veces <= 15; veces = veces + 1) {
+      System.out.print((int) (Math.random() * 10) + " ");
+    }
     System.out.println(" ");
     System.out.println(" ");
- 	}
+    System.out.println(" ");
+    System.out.println("Esto son números aleatorios del 1 al 10 => ");
+    System.out.println(" ");
+
+    for (int veces = 1; veces <= 15; veces = veces + 1) {
+      System.out.print((int) (Math.random() * 10 + 1) + " ");
+    }
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("Esto son números aleatorios del 0 al 10 => ");
+    System.out.println(" ");
+
+    for (int veces = 1; veces <= 15; veces = veces + 1) {
+      System.out.print((int) (Math.random() * 11) + " ");
+    }
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("Esto son números aleatorios del 0 al 6 => ");
+    System.out.println(" ");
+
+    for (int veces = 1; veces <= 15; veces = veces + 1) {
+      System.out.print((int) (Math.random() * 7) + " ");
+    }
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("Esto son números aleatorios del 50 al 60 => ");
+    System.out.println(" ");
+
+    for (int veces = 1; veces <= 15; veces = veces + 1) {
+      System.out.print(((int) (Math.random() * 11) + 50) + " ");
+
+    }
+    System.out.println(" ");
+    System.out.println(" ");
+  }
 }
