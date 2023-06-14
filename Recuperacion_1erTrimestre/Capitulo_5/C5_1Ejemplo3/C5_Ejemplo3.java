24d738a4ae2c7c6e86737b9ca9ef97eac44ee225
ArkanMB <baena.moyaangel@gmail.com>
2023-06-14 13:17:16 +0200

    Update

--- Recuperacion_1erTrimestre/Capitulo_5/C5_1Ejemplo3/C5_Ejemplo3.java
+++ Recuperacion_1erTrimestre/Capitulo_5/C5_1Ejemplo3/C5_Ejemplo3.java
@@ -1,9 +1,14 @@
 package C5_1Ejemplo3;
 
+import java.util.Scanner;
+
 /**
  * @file: C5_Ejemplo3.java
  * 
- * @info: 
+ * @info: Uso del bucle while pero introduciendo datos, el usuario tendría que
+ *        escribir exit para poder salir del programa si no seguirá pidiendo que
+ *        introduzcas algun valor, y lo que hará será imprimirte lo que
+ *        escribas.
  * 
  * @author: José Ángel Moya Baena
  */
@@ -12,7 +17,32 @@ public class C5_Ejemplo3 {
 
   public static void main(String[] args) {
 
-    
+    // Creamos un objeto de tipo Scanner importando la clase Scanner para poder
+    // introducir datos por teclado
+    Scanner s = new Scanner(System.in);
+
+    // Declaramos una variable para introducir la respuesta y será de tipo
+    // String(caracteres)
+    String respuesta = "";
+
+    // Este bucle te pedirá que introduzcas el valor exit para poder salir del
+    // programa es decir, mientras la respuesta no sea "exit", se seguirá reiterando
+    // en el bucle e imprimiendote lo que introduzcas
+    while (!respuesta.equals("exit")) {
+
+      // Le decimos al usuario lo que tiene que hacer
+      System.out.print("\nIntroduce un valor (escribe 'exit' para salir del programa) => ");
+
+      // Y leemos los datos introducidos
+      respuesta = s.nextLine();
+      
+      // Imprime la respuesta del usuario
+      System.out.println("\nHas puesto => " + respuesta); 
+    }
+
+    // Cerramos Scanner cuando termine el programa para evitar problemas de
+    // cualquier tipo
+    s.close();
 
   }
 
--- Recuperacion_1erTrimestre/Capitulo_5/C5_1Ejemplo4/C5_1Ejemplo4.java
+++ Recuperacion_1erTrimestre/Capitulo_5/C5_1Ejemplo4/C5_1Ejemplo4.java
@@ -3,7 +3,8 @@ package C5_1Ejemplo4;
 /**
  * @file: C5_Ejemplo4.java
  * 
- * @info: 
+ * @info: Uso del bucle do-while sencillo, vamos a hacer un contador del 1 hasta
+ *        el 5
  * 
  * @author: José Ángel Moya Baena
  */
@@ -12,7 +13,23 @@ public class C5_1Ejemplo4 {
 
   public static void main(String[] args) {
 
-    
+    // Declaramos una variable llamada contador iniciada en 1
+    int contador = 1;
+
+    // Este bucle do-while primero imprime y incrementa el contador y luego
+    // comprueba si el contador es menor o igual que 5 y si es true pues sale del
+    // bucle
+    do {
+
+      // Imprimimos por pantalla
+      System.out.println("\nContador => " + contador);
+
+      // Incrementamos contador
+      contador++;
+
+    } while (contador <= 5);
+
+System.out.println("\n");
 
   }
 
