c8655ab1ed3532ea9d85ed9936eaa9a693d09a47
ArkanMB <baena.moyaangel@gmail.com>
2023-06-15 11:38:47 +0200

    Update

--- Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej1/T5_3Ej1.java
+++ Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej1/T5_3Ej1.java
@@ -3,6 +3,8 @@
  * 
  * @statement: Realiza un programa que muestre por pantalla en cada caso, los
  *             siguientes valores de las variables a, b y c.
+ * 
+ * @author: José Ángel Moya Baena
  */
 public class T5_3Ej1 {
 
--- Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej2/T5_3Ej2.java
+++ Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej2/T5_3Ej2.java
@@ -1,5 +1,9 @@
 /**
- * T5_3Ej2
+ * @file: T5_3Ej2
+ * 
+ * @statement: 
+ * 
+ * @author: José Ángel Moya Baena
  */
 public class T5_3Ej2 {
 
