bcc47afe22398c88b40a93a6f35f2cfb46fb5059
ArkanMB <baena.moyaangel@gmail.com>
2023-06-15 12:53:27 +0200

    Update

--- Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej2/T5_3Ej2.java
+++ Recuperacion_1erTrimestre/Tarea5/T5_3/T5_3Ej2/T5_3Ej2.java
@@ -1,13 +1,64 @@
 /**
  * @file: T5_3Ej2
  * 
- * @statement: 
+ * @statement: Realizar un programa que muestre por pantalla el resultado de
+ *             cada una de las siguientes expresiones, suponiendo valores
+ *             iniciales de entero1 = 5 y entero2 = 7
  * 
  * @author: José Ángel Moya Baena
  */
 public class T5_3Ej2 {
 
   public static void main(String[] args) {
+
+    // Le explicamos al usuario lo que vamos a hacer
+    System.out.println();
+    System.out.printf("%1s %s", "", "Vamos a realizar una tabla de funciones");
+    System.out.println("\n");
+
+    // Ahora declaramos las variables que vamos a necesitar para los calculos
+    int entero1 = 5;
+    int entero2 = 7;
+    boolean a, b, d, e, f, g, h;
+    int c;
+
+    // Aqui tendríamos la tabla en la que vamos creando las filas con el espacio que
+    // queramos y alineados a la izquierda
+    // Intercaladamente iremos haciendo los calculos y concatenandolos a la tabla
+    System.out.printf("%-4s %-20s %-7s", "", "Instrucción", "");
+    System.out.println();
+
+    a = (entero1 > 5) && (entero2 + 3 < 10);
+    System.out.printf("%-4s %-20s %-7s", "A)", "a = b", a);
+    System.out.println();
+
+    b = (10 != entero1 + 5) || (20 > (entero2 + 5) * entero1 + 5);
+    System.out.printf("%-4s %-20s %-7s", "B)", "c = a", b);
+    System.out.println();
+
+    c = entero1 * entero2 + 10 + 4/2 - 2*4 +2;
+    System.out.printf("%-4s %-20s %-7s", "C)", "c = a", c);
+    System.out.println();
+
+    d = (10/2 - 1 == entero1) && (20 > entero2 + 13);
+    System.out.printf("%-4s %-20s %-7s", "D)", "c = a", d);
+    System.out.println();
+
+    e = (5-2 > 4) &&! (5 == 25/5);
+    System.out.printf("%-4s %-20s %-7s", "E)", "c = a", e);
+    System.out.println();
+
+    f = "J"=="j" || "S"=="s"; 
+    System.out.printf("%-4s %-20s %-7s", "F)", "c = a", f);
+    System.out.println();
+
+    g = (4*2 <= 8) || (2*2 < 5) && (4 > 3 + 1);
+    System.out.printf("%-4s %-20s %-7s", "G)", "c = a", g);
+    System.out.println();
+
+    h = (10+4 < 15-3) || (2*5 + 1 > 14 - 2*2);
+    System.out.printf("%-4s %-20s %-7s", "H)", "c = a", h);
+    System.out.println("\n");
 
   }
 
